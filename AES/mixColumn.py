cf3e7e44b080f72cd2b678afe3e46ad6c6dd0ace
Lifehunted <lasse@tostrup.dk>
2025-11-20 12:03:17 +0100

    Almost implemented galois fields

--- AES/mixColumn.py
+++ AES/mixColumn.py
@@ -1,4 +1,48 @@
 import numpy as np
+import math
+
+class gf28:
+    def __init__(self, value: int):
+        self.value = value
+        self.prime = 2
+        self.extensionDegree = 8
+        self.irreduciblePolynomial = 0b100011011 # x^8+x^4+x^3+x+1
+    
+    def __add__(self, other):
+        return gf28(int(self)^int(other))
+
+    def __sub__(self,other):
+        return gf28(int(self)^int(other))
+
+    def __int__(self):
+        return self.value
+    
+    def __str__(self):
+        bitString = bin(self.value).split('b')[1]
+        print(bitString)
+        finalString = ""
+        first = True
+        for exponent,value in list(zip(range(len(bitString)-1,-1,-1),bitString)):
+            if value == '1':
+                if not first:
+                    finalString += '+'
+                else:
+                    first = False
+                finalString += f" x^{exponent} "
+        return finalString
+
+    def __mul__(self, other):
+        mulProduct = gf28(int(self)*int(other))
+
+        while math.log2(int(mulProduct)) >= 8:
+            shiftBy = math.floor(math.log2(int(mulProduct)))-8
+            mulProduct -= (self.irreduciblePolynomial << shiftBy)
+
+        return mulProduct
+
+    def __rmul__(self,other):
+        return self.__mul__(other)
+
 
 def aes_mix_col(hex_input):
     aes_standard = np.matrix([
@@ -24,4 +68,8 @@ def aes_mix_col(hex_input):
     print(result)
     return result*aes_standard
 
-print(aes_mix_col(0x00112233445566778899aabbccddeeff))
+#print(aes_mix_col(0x00112233445566778899aabbccddeeff))
+
+print(
+    gf28(0x87) + gf28(0x6E) + (gf28(0x2) * gf28(0x46)) + (gf28(0x3) * gf28(0xA6))
+)
