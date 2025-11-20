9c7033ffc2c13abbdb9b576fea9819a817414f2e
Lifehunted <lasse@tostrup.dk>
2025-11-20 14:14:00 +0100

    small fix

--- AES/mixColumn.py
+++ AES/mixColumn.py
@@ -7,13 +7,13 @@ class GF2_8:
         # Prime: 2
         # Extension degree 8
         self.irreduciblePolynomial = 0b100011011 # x^8+x^4+x^3+x+1
-        self.value = modPoly(value)
+        self.value = value
 
     def __add__(self, other):
-        return GF2_8(int(self)^int(other))
+        return GF2_8(modPoly(int(self)^int(other)))
 
     def __sub__(self,other):
-        return GF2_8(int(self)^int(other))
+        return GF2_8(modPoly(int(self)^int(other)))
 
     def __int__(self):
         return self.value
@@ -38,28 +38,23 @@ class GF2_8:
         for digitPlace, value in enumerate(bin(int(other)).split('b')[1][::-1]): # i want the digits to come from smallest to biggest with the smallets digitPlace being 0
             if value == '1':
                 SoP ^= (self.value << digitPlace)
-        return GF2_8(SoP)
+        return GF2_8(modPoly(SoP))
 
     def __rmul__(self,other):
         return self.__mul__(other)
 
-def modPoly(poly : int | GF2_8):
+def modPoly(poly : int):
     """
     The examples showcase both multiplication AND modPoly at the same time
     Examples:
-    >>> str(modPoly(GF2_8(0b10100101) * GF2_8(0b110)))
+    >>> str(GF2_8(modPoly(GF2_8(0b10100101) * GF2_8(0b110))))
     'x^7 + x^6 + x^5 + x^4           + x^1 + x^0'
-    >>> str(modPoly(GF2_8(0b10101010) * GF2_8(0b110)))
+    >>> str(GF2_8(modPoly(GF2_8(0b10101010) * GF2_8(0b110))))
     'x^7 + x^6      + x^4                + x^0'
-    >>> str(modPoly(GF2_8(0b10111010) * GF2_8(0b110)))
+    >>> str(GF2_8(modPoly(GF2_8(0b10111010) * GF2_8(0b110))))
     'x^7      + x^5 + x^4                + x^0'
     """
-    if type(poly) == GF2_8:
-        while math.log2(int(poly)) >= 8:
-            shiftBy = math.floor(math.log2(int(poly)))-8
-            poly -= (0b100011011 << shiftBy)
-    else:
-        poly = int(poly)
+    poly = int(poly)
     if type(poly) == int:
         while math.log2(int(poly)) >= 8:
             shiftBy = math.floor(math.log2(int(poly)))-8
@@ -70,38 +65,42 @@ def modPoly(poly : int | GF2_8):
 
 print(doctest.testmod())
 
-class AES_Array:
-    def __init__(self,input):
-        self.aes_standard = [
-            [GF2_8(2),GF2_8(3),GF2_8(1),GF2_8(1)],
-            [GF2_8(1),GF2_8(2),GF2_8(3),GF2_8(1)],
-            [GF2_8(1),GF2_8(1),GF2_8(2),GF2_8(3)],
-            [GF2_8(3),GF2_8(1),GF2_8(1),GF2_8(2)]
-        ]
+def aes_mix_col(hex_input):
+    aes_standard = np.matrix([
+        [2,3,1,1],
+        [1,2,3,1],
+        [1,1,2,3],
+        [3,1,1,2]
+    ])
+    np.set_printoptions(formatter={'int':hex})
 
-    def __mul__(self, other):
-        return 2
+    np.set_printoptions(formatter={'int':hex})
+    input_bytes = hex_input.to_bytes(16)
+    input_bytes = bytearray(input_bytes)
 
-    def EncryptBlock(self, block:int):
-        blockString = hex(block).strip('x')[1]
-        if len(blockString) != 32:
-            raise("invalid input length")
-        self.table = [
-            [blockString[0:2],blockString[8:10],blockString[16:18],blockString[24:26]],
-            [blockString[2:4],blockString[10:12],blockString[18:20],blockString[26:28]],
-            [blockString[4:6],blockString[12:14],blockString[20:22],blockString[28:30]],
-            [blockString[6:8],blockString[14:16],blockString[22:24],blockString[30:]]
-        ]
-        print("Still lacks encryption")
+    input_bytes = hex_input.to_bytes(16)
+    input_bytes = bytearray(input_bytes)
+    result = np.matrix(
+        input_bytes
+    ) # creates a 1 dimeensional matrix
+    print()
+    result = result.reshape((4,4)) # makes the matrix 2 dimensional
+    result = np.flip(result,axis=1) # part 1 of correcting the matrix layout
+    result = np.rot90(result) # part 2 of correcting the matrix layout
 
-    def aes_mix_col(hex_input : list[list[GF2_8]]):
-        aes_standard = [
-            [GF2_8(2),GF2_8(3),GF2_8(1),GF2_8(1)],
-            [GF2_8(1),GF2_8(2),GF2_8(3),GF2_8(1)],
-            [GF2_8(1),GF2_8(1),GF2_8(2),GF2_8(3)],
-            [GF2_8(3),GF2_8(1),GF2_8(1),GF2_8(2)]
-        ]
+    result = np.matrix(
+        input_bytes
+    ) # creates a 1 dimeensional matrix
+    print()
+    result = result.reshape((4,4)) # makes the matrix 2 dimensional
+    result = np.flip(result,axis=1) # part 1 of correcting the matrix layout
+    result = np.rot90(result) # part 2 of correcting the matrix layout
+
+    print(result)
+    return result*aes_standard
 
 
+
+print(aes_mix_col(0x00112233445566778899aabbccddeeff))
 
 print()
