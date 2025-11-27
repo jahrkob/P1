2dcddcac145692fc68682b1fdd3f1d51da53ec65
Lifehunted <lasse@tostrup.dk>
2025-11-27 11:46:52 +0100

    Update test.py

--- AES/test.py
+++ AES/test.py
@@ -30,10 +30,10 @@ def singleRound(text:str,initialKey:int|str):
     #print(hex(e))
     return e
 
-def AES(text:str,initialKey:int,debug:bool = False):
+def AES_encrypt(text:str,initialKey:int,debug:bool = False):
     """
     Examples:
-    >>> AES('Two One Nine Two',0x5468617473206D79204B756E67204675,debug=True)
+    >>> AES_encrypt('Two One Nine Two',0x5468617473206D79204B756E67204675,debug=True)
     0x5847088b15b61cba59d4e2e8cd39dfce
     0x43c6a9620e57c0c80908ebfe3df87f37
     0x29c3505f571420f6402299b31a02d73a
@@ -65,15 +65,17 @@ def AES(text:str,initialKey:int,debug:bool = False):
         encryptedText += a.to_bytes(16)
     return base64.b64encode(encryptedText)
 
-def unBase64Encode(b64EncodedText):
+def base64Decode(b64EncodedText):
     """
     Examples:
-    >>> list(hex(i) for i in unBase64Encode(b'KcNQX1cUIPZAIpmzGgLXOrPkbxG6jSuXwYdpRJqJ6Gg='))
+    >>> list(hex(i) for i in base64Decode(b'KcNQX1cUIPZAIpmzGgLXOrPkbxG6jSuXwYdpRJqJ6Gg='))
     ['0x29c3505f571420f6402299b31a02d73a', '0xb3e46f11ba8d2b97c18769449a89e868']
     """
     byteList = base64.b64decode(b64EncodedText)
     return list(listToInt(list(byteList[i:i+16]),16) for i in range(0,len(byteList),16))
 
+def AES_decrypt(base64EncodedText):
+    encryptedText = base64Decode(base64EncodedText)
 
 
 
@@ -82,4 +84,4 @@ if __name__ == '__main__':
 
     key = 0x6162636465666768696a6b6C6D6E6F70
     initialText = 'Two One Nine Two'
-    print(AES(initialText,key))
+    print(AES_encrypt(initialText,key))
