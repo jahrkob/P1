58273610bab416b78c854b9169b6f29879d1a89a
Lifehunted <lasse@tostrup.dk>
2025-12-01 12:54:13 +0100

    128-bit things are ASS

--- AES/Arduino_AES/Arduino_AES.ino
+++ AES/Arduino_AES/Arduino_AES.ino
@@ -25,8 +25,6 @@ void mixCol(int col[4], int out[4]) {
   out[3] = GFadd(GFmul(col[0],3)   ,col[1]             ,col[2]             ,GFmul(col[3],2));
 }
 
-
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int mainFromCpp() {
@@ -34,10 +32,29 @@ int mainFromCpp() {
     if (testModPoly()) {Serial.println("modPoly tests passed");} else {Serial.println("modPoly tests failed");}
     if (testGF()) {Serial.println("GF2_8 tests passed");} else {Serial.println("GF2_8 tests failed");}
     if (testMixCol()) {Serial.println("mixCol tests passed");} else {Serial.println("mixCol tests failed");}
-    int list[] = {1,2,3,4};
-    int mixedCol[4];
-    mixCol(list,mixedCol);
-    Serial.println(mixedCol[0]);
+    testAESMixCol();
+}
+
+void AESMixCol(int in[16], int out[16]) {
+    out[0] = GFadd(GFmul(in[0],2)   ,GFmul(in[1],3)    ,in[2]             ,in[3]);
+    out[1] = GFadd(in[0]            ,GFmul(in[1],2)    ,GFmul(in[2],3)    ,in[3]);
+    out[2] = GFadd(in[0]            ,in[1]             ,GFmul(in[2],2)    ,GFmul(in[3],3));
+    out[3] = GFadd(GFmul(in[0],3)   ,in[1]             ,in[2]             ,GFmul(in[3],2));
+
+    out[4] = GFadd(GFmul(in[4],2)   ,GFmul(in[5],3)    ,in[6]             ,in[7]);
+    out[5] = GFadd(in[4]            ,GFmul(in[5],2)    ,GFmul(in[6],3)    ,in[7]);
+    out[6] = GFadd(in[4]            ,in[5]             ,GFmul(in[6],2)    ,GFmul(in[7],3));
+    out[7] = GFadd(GFmul(in[4],3)   ,in[5]             ,in[6]             ,GFmul(in[7],2));
+
+    out[8] = GFadd(GFmul(in[8],2)   ,GFmul(in[9],3)    ,in[10]             ,in[11]);
+    out[9] = GFadd(in[8]            ,GFmul(in[9],2)    ,GFmul(in[10],3)    ,in[11]);
+    out[10] = GFadd(in[8]           ,in[9]             ,GFmul(in[10],2)    ,GFmul(in[11],3));
+    out[11] = GFadd(GFmul(in[8],3)  ,in[9]             ,in[10]             ,GFmul(in[11],2));
+
+    out[12] = GFadd(GFmul(in[12],2)  ,GFmul(in[13],3)    ,in[14]             ,in[15]);
+    out[13] = GFadd(in[12]           ,GFmul(in[13],2)    ,GFmul(in[14],3)    ,in[15]);
+    out[14] = GFadd(in[12]           ,in[13]             ,GFmul(in[14],2)    ,GFmul(in[15],3));
+    out[15] = GFadd(GFmul(in[12],3)  ,in[13]             ,in[14]             ,GFmul(in[15],2));
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -128,6 +145,22 @@ bool testMixCol() {
         }
     }
     return returnValue;
+}
+
+bool testAESMixCol() {
+    int in[16] =  {0x63, 0x2f, 0xaf, 0xa2, 0xeb, 0x93, 0xc7, 0x20, 0x9f, 0x92, 0xab, 0xcb, 0xa0, 0xc0, 0x30, 0x2b};
+    int out[16];
+    int expectedOut[16] = {0xba, 0x75, 0xf4, 0x7a, 0x84, 0xa4, 0x8d, 0x32, 0xe8, 0x8d, 0x06, 0x0e, 0x1b, 0x40, 0x7d, 0x5d};
+    AESMixCol(in, out);
+    //Serial.println(expectedOut);
+    //Serial.println(out);
+    if (out == expectedOut) {
+        Serial.println("AESMixCol tests passed");
+        return true;
+    } else {
+        Serial.println("FAILED: AESMixCol tests failed");
+        return false;
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////
